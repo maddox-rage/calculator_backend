064095794a486fb67c07fa42a7757f443e74d025
zakhar <zahar.ava@mail.ru>
2024-09-10 11:23:24 +0800

    add cors policy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { hostname } from 'os';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, 
+  });
   app.setGlobalPrefix('api');
   await app.listen(80, '0.0.0.0');
 }
