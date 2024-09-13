1aba8a189eb9bf0efea7ec7932207890f2d351b7
zakhar <zahar.ava@mail.ru>
2024-09-13 15:14:15 +0800

    changed cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,9 +4,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173', 
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true, 
+    credentials: true,
   });
   app.setGlobalPrefix('api');
   await app.listen(80, '0.0.0.0');
